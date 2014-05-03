13caa39181a864fed42e81bb1a2519e574d0308c
Jaydeep <jaydeep.wagh@changer.nl>
2014-05-03 23:01:27 +0530

    handle UI updates when audio play completes

--- LibAudioWife/src/nl/changer/audiowife/AudioPlayerController.java
+++ LibAudioWife/src/nl/changer/audiowife/AudioPlayerController.java
@@ -21,6 +21,8 @@ public class AudioPlayerController {
 	
 	private static Handler mHandler = new Handler();;
 	
+	/***Initially the audio starts from the beginning.
+	So initialized to 0*/
 	private static int mCurrentPlayTime = 0;
 	private static SeekBar mSeekBar;
 	private static TextView mPlaybackTime;
@@ -29,15 +31,122 @@ public class AudioPlayerController {
 
 	private static MediaPlayer mMediaPlayer = new MediaPlayer();;
 	/***
-	 * Maybe audio uri
+	 * Audio URI
 	 ****/
 	private static Uri mUri;
 	
+	private static Runnable mUpdateTime = new Runnable() {
+	      public void run() {
+	    	  
+	    	  long totalDuration = 0;
+	    	  int currentTime = 0;
+	    	  
+	    	  if(mMediaPlayer != null) {
+	    		  try {
+		    		  totalDuration = mMediaPlayer.getDuration();
+		    		  currentTime = mMediaPlayer.getCurrentPosition();
+		    	  } catch (IllegalStateException e) {
+		    		  e.printStackTrace();
+		    	  } catch (Exception e) {
+		    		  e.printStackTrace();
+		    	  }
+	    	  }
+	    	  
+	    	  StringBuilder playbackStr = new StringBuilder();
+	    	  
+	    	  // set the current time
+	    	  // its ok to show 00:00 in the UI
+  		  playbackStr.append(String.format("%02d:%02d",
+	            TimeUnit.MILLISECONDS.toMinutes((long) currentTime),
+	            TimeUnit.MILLISECONDS.toSeconds((long) currentTime) - 
+	            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
+	            toMinutes((long) currentTime)))
+	         );	    		  
+	    	  
+	    	  playbackStr.append("/");
+	    	  
+	    	  // set total time as the audio is being played
+	    	  if(totalDuration != 0) {
+	    		  playbackStr.append(String.format("%02d:%02d",
+		            TimeUnit.MILLISECONDS.toMinutes((long) totalDuration),
+		            TimeUnit.MILLISECONDS.toSeconds((long) totalDuration) - 
+		            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
+		            toMinutes((long) totalDuration)))
+		          );  
+	    	  } else
+	    		  DebugLog.w("Something strage this audio track duration in zero");
+	    	  
+	    	  mPlaybackTime.setText(playbackStr);
+	         
+	    	  DebugLog.i("current: " + currentTime + " total: " + totalDuration);
+	    	  
+	    	  if(mHandler != null && mMediaPlayer.isPlaying() ) {
+	    		  mSeekBar.setProgress((int) currentTime);
+	    		  mHandler.postDelayed(this, AUDIO_PROGRESS_UPDATE_TIME);
+	    	  } else {
+	    		  // DO NOT update UI if the player is paused
+	    	  }
+	      }
+	};
+	
+
+	
 	public static void play(Context ctx) {
+        
+        int currentTime = mMediaPlayer.getCurrentPosition();
+        mSeekBar.setProgress((int)currentTime);
+        
+        mHandler.postDelayed(mUpdateTime, AUDIO_PROGRESS_UPDATE_TIME);
+        
+        mMediaPlayer.seekTo(mCurrentPlayTime);
+        
+        mMediaPlayer.start();
+        
+        setPausable();
+	}
+
+	public static void pause() {
+      mMediaPlayer.pause();
+      mCurrentPlayTime = mSeekBar.getProgress();
+      setPlayable();
+	}
+	
+	private static void setPlayable() {
+		if(mPlayButton != null)
+			mPlayButton.setVisibility(View.VISIBLE);
 		
-		if(mPlayButton != null) mPlayButton.setVisibility(View.GONE);
-		if(mPauseButton != null) mPauseButton.setVisibility(View.VISIBLE);
+	    if(mPauseButton != null)
+	    	mPauseButton.setVisibility(View.GONE);
+	}
+	
+	private static void setPausable() {
+		if(mPlayButton != null)
+			mPlayButton.setVisibility(View.GONE);
+		if(mPauseButton != null) 
+			mPauseButton.setVisibility(View.VISIBLE);
+	}
+ 
+	/***
+	 * Initialize the audio player
+	 * 
+	 ****/
+	public static void init(Context ctx, Uri uri, SeekBar seekBar, View playBtn, View pauseBtn, TextView playTime) {
+		mUri = uri;
+		mSeekBar = seekBar;
+		mPlayButton = playBtn;
+		mPauseButton = pauseBtn;
+		mPlaybackTime = playTime;
 		
+		initPlayer(ctx);
+        
+		initMediaSeekBar();
+	}
+	
+	/****
+	 * Initialize and prepare the audio player
+	 * 
+	 ****/
+	private static void initPlayer(Context ctx) {
 		mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         
         try {
@@ -50,6 +159,8 @@ public class AudioPlayerController {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         
         try {
@@ -58,100 +169,29 @@ public class AudioPlayerController {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         
-        mMediaPlayer.start();
+        mMediaPlayer.setOnCompletionListener( new MediaPlayer.OnCompletionListener() {
+			
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+    		  	// set UI when audio finished playing
+				DebugLog.i("Finished playing audio");
+    			mCurrentPlayTime = 0;
+    			mSeekBar.setProgress((int) mCurrentPlayTime);
+    			setPlayable();
+			}
+		});
+	}
+
+	private static void initMediaSeekBar() {
+		
+		// update seekbar
         long finalTime = mMediaPlayer.getDuration();
         mSeekBar.setMax((int) finalTime);
         
-        int currentTime = mMediaPlayer.getCurrentPosition();
-        
-        mSeekBar.setProgress((int)currentTime);
-        
-        mHandler.postDelayed(mUpdateTime, AUDIO_PROGRESS_UPDATE_TIME);
-        mPlayButton.setEnabled(true);
-        
-        mMediaPlayer.seekTo(mCurrentPlayTime);
-        
-        mMediaPlayer.start();
-	}
-	
-	private static Runnable mUpdateTime = new Runnable() {
-	      public void run() {
-	    	  
-	    	  long duration = 0;
-	    	  int currentTime = 0;
-	    	  
-	    	  if(mMediaPlayer != null) {
-	    		  try {
-		    		  duration = mMediaPlayer.getDuration();
-		    		  currentTime = mMediaPlayer.getCurrentPosition();
-		    	  } catch (IllegalStateException e) {
-		    		  e.printStackTrace();
-		    	  }
-	    		  
-	    		  // set UI when audio finished playing
-	    		  if(currentTime == duration) {
-	    			  mCurrentPlayTime = 0;
-	    			  setPlayable();
-	    		  }
-	    	  }
-	    	  
-	    	  StringBuilder playbackStr = new StringBuilder();
-	    	  
-	    	  // set the current time
-	    	  // its ok to show 00:00 in the UI
-    		  playbackStr.append(String.format("%02d:%02d",
-	            TimeUnit.MILLISECONDS.toMinutes((long) currentTime),
-	            TimeUnit.MILLISECONDS.toSeconds((long) currentTime) - 
-	            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
-	            toMinutes((long) currentTime)))
-	         );	    		  
-	    	  
-	    	  playbackStr.append("/");
-	    	  
-	    	  // set total time as the audio is being played
-	    	  if(duration != 0) {
-	    		  playbackStr.append(String.format("%02d:%02d",
-  		            TimeUnit.MILLISECONDS.toMinutes((long) duration),
-  		            TimeUnit.MILLISECONDS.toSeconds((long) duration) - 
-  		            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
-  		            toMinutes((long) duration)))
-  		          );  
-	    	  } else
-	    		  DebugLog.w("Something strage this audio track duration in zero");
-	    	  
-	    	  mPlaybackTime.setText(playbackStr);
-	    	  
-	    	  mSeekBar.setProgress((int) currentTime);
-	         
-	    	  if(mHandler != null && mMediaPlayer.isPlaying() )
-	    		  mHandler.postDelayed(this, AUDIO_PROGRESS_UPDATE_TIME);
-	      }
-	};
-	
-	public static void pause() {
-      mMediaPlayer.pause();
-      mCurrentPlayTime = mSeekBar.getProgress();
-      setPlayable();
-	}
-	
-	private static void setPlayable() {
-		if(mPlayButton != null) mPlayButton.setVisibility(View.VISIBLE);
-	    if(mPauseButton != null) mPauseButton.setVisibility(View.GONE);
-	}
- 
-	public static void initAudioPlayer(Uri uri, SeekBar seekBar, View playBtn, View pauseBtn, TextView playTime) {
-		mUri = uri;
-		mSeekBar = seekBar;
-		mPlayButton = playBtn;
-		mPauseButton = pauseBtn;
-		mPlaybackTime = playTime;
-		
-		initMediaSeekBar();
-	}
-	
-	private static void initMediaSeekBar() {
 		mSeekBar.setOnSeekBarChangeListener( new SeekBar.OnSeekBarChangeListener() {
 			
 			@Override
@@ -179,6 +219,5 @@ public class AudioPlayerController {
 			mMediaPlayer = null;
 			mHandler = null;	
 		}
-	
 	}
 }
--- README.md
+++ README.md
@@ -8,3 +8,15 @@ Credits
 ==========
 
 http://www.tutorialspoint.com/android/android_mediaplayer.htm
+
+
+Why this project?
+====================
+
+
+Getting started
+====================
+
+
+Why the name 'AudioWife'?
+=========================
